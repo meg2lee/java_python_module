86fe1f2eabad4a4ce32465eb780cbc65609e9ed9
Megan Lee <74307093+meg2lee@users.noreply.github.com>
2020-12-26 23:00:25 +0900

    Update PieChartCreator.java
    add comments

--- PieChartCreator.java
+++ PieChartCreator.java
@@ -38,19 +38,22 @@ public class PieChartCreator {
 			for(int i = 0 ; i < mfiles.length ; i++) {
 	            mfiles[i].transferTo(new File(savePath+"/"+mfiles[i].getOriginalFilename()));
 	        }
-			
-			ProcessBuilder pb=new ProcessBuilder("python",pymodule,savePath+mfiles[0].getOriginalFilename(),imgPath).inheritIO();
-	        Process p=pb.start();
-	        p.waitFor();
+		
+		/*java단에서 python을 함께 실행하도록 하는 ProcessBuilder 생성
+		* 순서대로 python.exe, 실행할.py파일, .py에서 참고할 csv파일 위치(first argument), .py 실행후 이미지 저장할 위치(second argument)*/
+		ProcessBuilder pb=new ProcessBuilder("python",pymodule,savePath+mfiles[0].getOriginalFilename(),imgPath).inheritIO();
+	        Process p=pb.start(); // process 실행
+	        p.waitFor(); // 실행완료될때까지 대기
+		/*bianry파일->tex파일->한 행씩 읽어옴*/
 	        BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
-	        String line="";
-	        while((line=bfr.readLine())!=null) {
-	           System.out.println(line);
+	        String line=""; // 빈 문자열 준비
+	        while((line=bfr.readLine())!=null) { //null 나올때까지 읽어온 한 행씩 저장
+	           System.out.println(line); // .py 전체코드출력(최종적으로 piechart 생성 후, 저장)
 	        }
         } catch(Exception e) {
 	    	e.printStackTrace();  
         }
-        return isCreated;
+        return isCreated; // boolean값 return
    }
 	
 }
